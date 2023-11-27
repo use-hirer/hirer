851112b80a30da57c1093e16610a44268cef4c05
Nick <nick.mandylas@gmail.com>
2023-11-27 21:24:01 +1100

    fix: scroll issue on content

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
     <>
       <div className="h-screen w-full bg-zinc-50 flex">
         <NavigationMenu />
-        <div className="flex-auto bg-white mt-3 rounded-tl-2xl rounded-bl-2xl shadow-sm border-zinc-950/5 border p-4 md:ml-[250px]">
+        <div className="overflow-y-scroll flex-auto bg-white mt-3 rounded-tl-2xl shadow-sm border-zinc-950/5 border p-4 md:ml-[250px]">
           {children}
         </div>
       </div>
