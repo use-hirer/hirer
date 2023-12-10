a9b9e90d41fc2c17282e8c8f3dce6fbf4b131d1f
Nick <nick.mandylas@gmail.com>
2023-12-10 16:05:05 +1100

    update: login design

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { Sparkle } from "@phosphor-icons/react/dist/ssr";
 import { getServerSession } from "next-auth";
 import { RedirectType, redirect } from "next/navigation";
 import { UserAuthForm } from "../../../components/auth/use-auth-form";
@@ -25,25 +26,28 @@ export default async function AuthenticationPage() {
         <Link
           href="/examples/authentication"
           className={cn(
-            buttonVariants({ variant: "ghost" }),
+            buttonVariants({ variant: "outline" }),
             "absolute right-4 top-4 md:right-8 md:top-8"
           )}
         >
-          Login
+          Sign Up 👋
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-extrabold">
+          <div className="relative z-20 flex items-center text-lg font-extrabold gap-1">
+            <Sparkle color="white" />
             Hirer
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                Did you know? Hirer utilizes AI search retrieval to swiftly
+                search every detail in applicant resumes, ensuring no
+                information is overlooked.
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              <footer className="text-sm text-orange-500">
+                usehirer.com tip #4
+              </footer>
             </blockquote>
           </div>
         </div>
@@ -51,10 +55,10 @@ export default async function AuthenticationPage() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                Login to Hirer
               </h1>
               <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+                Enter your email below to access your account
               </p>
             </div>
             <UserAuthForm />
