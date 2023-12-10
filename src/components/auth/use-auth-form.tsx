9aaafdf979f334e82e28b89ed670678f33e5bbb8
Nick <nick.mandylas@gmail.com>
2023-12-10 15:46:08 +1100

    fix: incorrect bg divider color

--- src/components/auth/use-auth-form.tsx
+++ src/components/auth/use-auth-form.tsx
@@ -59,7 +59,9 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
           <span className="w-full border-t" />
         </div>
         <div className="relative flex justify-center text-xs uppercase">
-          <span className="px-2 text-muted-foreground">Or continue with</span>
+          <span className="bg-white px-2 text-muted-foreground">
+            Or continue with
+          </span>
         </div>
       </div>
       <Button
