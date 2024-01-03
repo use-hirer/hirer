84ca10ca2f19086a229cd004459180d9ca66635a
Nick <nick.mandylas@gmail.com>
2024-01-04 07:23:27 +1100

    feat: analytics dashboard cards

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
     <>
       <div className="h-screen w-full bg-zinc-50 flex">
         <NavigationMenu />
-        <div className="overflow-y-scroll flex-auto bg-white mt-3 rounded-tl-2xl shadow-sm border-zinc-950/5 border p-4 md:ml-[250px]">
+        <div className="overflow-y-scroll flex-auto bg-white mt-[7px] rounded-tl-2xl shadow-sm border-zinc-950/5 border p-4 md:ml-[250px]">
           {children}
         </div>
       </div>
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { List } from "@phosphor-icons/react/dist/ssr";
 // import { Metadata } from "next";
 import { signOut } from "next-auth/react";
@@ -18,7 +20,28 @@ export default function DashboardPage() {
         >
           <List weight="bold" size={20} />
         </button>
-        <div className="font-extrabold text-xl">Home</div>
+        <div className="w-full">
+          <div className="font-extrabold text-xl">Dashboard</div>
+          <Separator className="mt-2 mb-4" />
+          <div className="grid grid-cols-4 gap-4">
+            <Card className="shadow-none rounded p-4 border-neutral-200">
+              <div className="font-bold text-sm">Vistors last 30 days</div>
+              <div className="text-xs font-light text-zinc-500">
+                Last 4 weeks
+              </div>
+              <div className="text-3xl font-extrabold pt-2">3277</div>
+            </Card>
+            <Card className="shadow-none rounded p-4 border-neutral-200">
+              2
+            </Card>
+            <Card className="shadow-none rounded p-4 border-neutral-200">
+              3
+            </Card>
+            <Card className="shadow-none rounded p-4 border-neutral-200">
+              4
+            </Card>
+          </div>
+        </div>
       </div>
     </>
   );
