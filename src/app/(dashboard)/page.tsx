13390e954e6c631b0c0ec0900eddfe5a08eca208
Nick <nick.mandylas@gmail.com>
2024-01-04 11:59:57 +1100

    add: jobs + candidates table to home page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,9 +2,10 @@
 
 import KPICard from "@/components/dashboard/kpi-card";
 import { Separator } from "@/components/ui/separator";
-import { List } from "@phosphor-icons/react/dist/ssr";
+import { ArrowRight, List } from "@phosphor-icons/react/dist/ssr";
 // import { Metadata } from "next";
 import { signOut } from "next-auth/react";
+import Link from "next/link";
 
 // export const metadata: Metadata = {
 //   title: "Hirer: Dashboard",
@@ -23,7 +24,7 @@ export default function DashboardPage() {
         <div className="w-full">
           <div className="font-extrabold text-xl">Dashboard</div>
           <Separator className="mt-2 mb-4" />
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2">
             <KPICard
               increase="positive"
               percentageChange={3.7}
@@ -56,6 +57,58 @@ export default function DashboardPage() {
               title="Total Followers"
               deltaType="unchanged"
             />
+          </div>
+          <div className="mt-4">
+            <div className="flex gap-2 items-center">
+              <div className="font-bold text-xl">Jobs</div>
+              <Link
+                href={"/jobs"}
+                className="flex items-center font-light text-xs text-neutral-700 gap-1"
+              >
+                <div>view all jobs</div>
+                <ArrowRight />
+              </Link>
+            </div>
+            {/* <div className="bg-gray-100/50 text-gray-600 border rounded-sm mt-2">
+              <Table>
+                <TableHeader className="text-gray-600">
+                  <TableRow className="w-full border-none ">
+                    <TableHead>Job Title</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Location</TableHead>
+                    <TableHead>Department</TableHead>
+                    <TableHead>Hiring Manager</TableHead>
+                  </TableRow>
+                </TableHeader>
+              </Table>
+            </div>
+            <div className="mt-2 border rounded-sm">
+              <Table>
+                <TableBody className="text-gray-600">
+                  <TableRow className="w-full border-none">
+                    <TableCell className="font-bold text-black">
+                      Senior Product Designer
+                    </TableCell>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Location</TableHead>
+                    <TableHead>Department</TableHead>
+                    <TableHead>Hiring Manager</TableHead>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </div> */}
+          </div>
+          <div className="mt-4">
+            <div className="flex gap-2 items-center">
+              <div className="font-bold text-xl">Candidates</div>
+              <Link
+                href={"/candidates"}
+                className="flex items-center font-light text-xs text-neutral-700 gap-1"
+              >
+                <div>view all candidates</div>
+                <ArrowRight />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
