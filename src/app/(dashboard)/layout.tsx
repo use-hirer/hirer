89f02d3dfbc3db998f62a84437d044103ea852e0
Nick <nick.mandylas@gmail.com>
2024-01-12 16:16:29 +0900

    feat: menu toggle now working

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,18 +1,21 @@
 import NavigationMenu from "@/components/menu/menu";
 import Notifications from "@/components/menu/notifications";
+import { authOptions } from "@/lib/auth";
 import { List } from "@phosphor-icons/react/dist/ssr";
+import { getServerSession } from "next-auth";
+import ContentShell from "./content-shell";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const isCollapsed = false;
+  const session = await getServerSession(authOptions);
 
   return (
     <>
       <div className="h-screen w-full bg-zinc-50 flex flex-col lg:flex-row">
-        <NavigationMenu collapsed={isCollapsed} />
+        <NavigationMenu userSession={session} />
         <div className="lg:hidden px-4 pt-2 flex items-center justify-between">
           <div className="flex items-center gap-2">
             <List weight="bold" size={20} />
@@ -20,13 +23,7 @@ export default function DashboardLayout({
           </div>
           <Notifications />
         </div>
-        {/* <NavCollapseButton /> */}
-        <div
-          data-collapsed={isCollapsed}
-          className="overflow-y-scroll flex-auto bg-white mt-[7px] lg:rounded-tl-2xl shadow-sm border-zinc-950/5 border p-4 lg:ml-[250px] lg:data-[collapsed=true]:ml-[60px]"
-        >
-          {children}
-        </div>
+        <ContentShell>{children}</ContentShell>
       </div>
     </>
   );
--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,56 +1,57 @@
-"use server";
+"use client";
 
-import { authOptions } from "@/lib/auth";
+import { useCollapse } from "@/context/CollapseContext";
 import { Sparkle } from "@phosphor-icons/react/dist/ssr";
-import { getServerSession } from "next-auth";
-import Link from "next/link";
+import { Session } from "next-auth";
 import { Separator } from "../ui/separator";
 import BottomMenuLayout from "./bottom-layout";
 import NavUser from "./nav-user";
 import { TeamSelect } from "./team-select";
 import TopMenuLayout from "./top-layout";
 
-const NavigationMenu: React.FC<{ collapsed: boolean }> = async ({
-  collapsed,
-}) => {
-  const session = await getServerSession(authOptions);
+const NavigationMenu: React.FC<{
+  userSession: Session | null;
+}> = ({ userSession }) => {
+  const { isCollapsed, toggleCollapse } = useCollapse();
 
   return (
     <>
       <nav
-        data-collapsed={collapsed}
+        data-collapsed={isCollapsed}
         className="hidden lg:flex flex-shrink-0 flex-col top-0 left-0 justify-between h-screen fixed w-[250px] pt-5 px-3 overflow-y-auto scroll-bar data-[collapsed=true]:w-[60px]"
       >
         <div>
-          <Link
-            data-collapsed={collapsed}
+          <div
+            data-collapsed={isCollapsed}
             className="flex items-center data-[collapsed=true]:justify-center select-none"
-            href="/"
+            onClick={toggleCollapse}
           >
             <div className="flex items-center justify-center w-7 h-7 bg-black text-white rounded-full text-md font-extrabold">
               <Sparkle />
             </div>
-            {!collapsed && <div className="font-bold text-xl ml-1">Hirer</div>}
-          </Link>
+            {!isCollapsed && (
+              <div className="font-bold text-xl ml-1">Hirer</div>
+            )}
+          </div>
           <Separator className="my-3" />
           {/* <Input className="bg-white h-8" placeholder="Search" /> */}
-          {!collapsed && (
+          {!isCollapsed && (
             <>
               <TeamSelect />
               <Separator className="my-3" />
             </>
           )}
-          <TopMenuLayout collapsed={collapsed} />
+          <TopMenuLayout collapsed={isCollapsed} />
         </div>
         <div className="mb-3 pt-10">
-          <BottomMenuLayout collapsed={collapsed} />
+          <BottomMenuLayout collapsed={isCollapsed} />
           <Separator className="mt-2 mb-3" />
           <NavUser
-            avatarUrl={session?.user?.image || ""}
-            name={session?.user?.name as string}
-            email={session?.user?.email as string}
+            avatarUrl={userSession?.user?.image || ""}
+            name={userSession?.user?.name as string}
+            email={userSession?.user?.email as string}
             role="Admin"
-            collapsed={collapsed}
+            collapsed={isCollapsed}
           />
         </div>
       </nav>
