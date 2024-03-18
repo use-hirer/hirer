ed977b55ef07fba5c27f14a1519d5cd0c3c0cd2b
Nick <nick.mandylas@gmail.com>
2024-03-18 21:53:17 +1100

    fix(console): gitignore is broken

---
+++ apps/console/src/app/onboarding/page.tsx
@@ -0,0 +1,3 @@
+export default function OnboardingPage() {
+  return <div>hello there!</div>;
+}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 4000, '0.0.0.0');
 }
 
 bootstrap();
