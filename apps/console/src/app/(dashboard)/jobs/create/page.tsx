fb3eebe5e661edaf6b4512bac92a7855cc7d8969
Nick <nick.mandylas@gmail.com>
2024-03-20 23:42:51 +1100

    add(console): job create form

--- apps/console/src/app/(dashboard)/jobs/create/page.tsx
+++ apps/console/src/app/(dashboard)/jobs/create/page.tsx
@@ -1,5 +1,11 @@
-import { JobsTable } from "@console/components/tables/jobs-table";
+import JobCreateForm from "@console/components/forms/job-create-form";
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@console/components/ui/alert";
 import { Separator } from "@console/components/ui/separator";
+import { ReadCvLogo } from "@phosphor-icons/react/dist/ssr";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,7 +18,20 @@ export default function JobsPage() {
       <div className="w-full">
         <div className="font-extrabold text-xl">Create a Job</div>
         <Separator className="my-2" />
-        <JobsTable />
+        <div className="my-4 text-sm">
+          <Alert>
+            <ReadCvLogo className="h-4 w-4" />
+            <AlertTitle>You&apos;re on your way!</AlertTitle>
+            <AlertDescription>
+              After creating a job, you&apos;ll be directed to the configuration
+              page to set AI evaluation criteria, form submission questions, and
+              more.
+            </AlertDescription>
+          </Alert>
+        </div>
+        <div>
+          <JobCreateForm />
+        </div>
       </div>
     </div>
   );
---
+++ apps/console/src/components/forms/job-create-form.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+
+const jobFormSchema = z.object({
+  position: z.string().min(1, "Every job needs a title."),
+  location: z
+    .string()
+    .min(1, "A location is required, either remote or place(s)."),
+  description: z.string().min(1, "Job description is required."),
+});
+
+type JobFormValues = z.infer<typeof jobFormSchema>;
+
+interface JobCreateFormProps {}
+
+const JobCreateForm: React.FC<JobCreateFormProps> = () => {
+  const form = useForm<JobFormValues>({
+    resolver: zodResolver(jobFormSchema),
+    mode: "onChange",
+    defaultValues: {
+      position: "",
+      location: "",
+      description: "",
+    },
+  });
+
+  const [content, setContent] = useState(null);
+
+  function onSubmit(data: JobFormValues) {
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            name="position"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Job Title <span className="text-red-600">*</span>
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="Software Engineer" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            name="location"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Location <span className="text-red-600">*</span>
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="Melbourne, Australia" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="description"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex justify-between items-center">
+                  <FormLabel>
+                    Job description <span className="text-red-600">*</span>
+                  </FormLabel>
+                  <Button size={"sm"}>Generate with AI</Button>
+                </div>
+                <FormControl>
+                  <Textarea
+                    placeholder="We are seeking a talented Software Engineer to join our dynamic team in building cutting-edge software solutions. As a Software Engineer, you will play a crucial role in designing, developing, and maintaining high-quality software applications."
+                    className="min-h-32"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+                <FormDescription>
+                  Struggling to write something down? Click the &apos;Generate
+                  with AI&apos; button.
+                </FormDescription>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Create Job</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default JobCreateForm;
