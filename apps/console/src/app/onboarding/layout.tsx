32582d40338e8508c2f1d0bc689bf519af402e15
Nick <nick.mandylas@gmail.com>
2024-03-26 22:43:14 +1100

    fix(console): set session null

--- apps/console/src/app/onboarding/layout.tsx
+++ apps/console/src/app/onboarding/layout.tsx
@@ -1,13 +1,8 @@
-import { authOptions } from "@console/lib/auth";
-import { getServerSession } from "next-auth";
-
 export default async function OnboardingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
-
   return (
     <>
       <div className="h-screen w-full bg-zinc-50 flex flex-col items-center justify-center">
