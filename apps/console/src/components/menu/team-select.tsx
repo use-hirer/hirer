f60004d1cfdb928cd5064d2e9916aa3533271113
Nick <nick.mandylas@gmail.com>
2024-03-27 18:43:02 +1100

    fix(console): team select

--- apps/console/src/components/menu/team-select.tsx
+++ apps/console/src/components/menu/team-select.tsx
@@ -87,7 +87,7 @@ export default function TeamSwitcher({ className }: TeamSwitcherProps) {
             role="combobox"
             aria-expanded={open}
             aria-label="Select a team"
-            className={cn("w-full justify-between bg-white", className)}
+            className={cn("w-full px-3 justify-between bg-white", className)}
           >
             <Avatar className="mr-2 h-5 w-5">
               <AvatarImage
@@ -101,7 +101,7 @@ export default function TeamSwitcher({ className }: TeamSwitcherProps) {
             <CaretSortIcon className="ml-auto h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
+        <PopoverContent className="w-[225px] p-0">
           <Command>
             <CommandList>
               <CommandInput placeholder="Search team..." />
