432fe9bac0fd6467f833bf2d7527753f75071768
Nick <nick.mandylas@gmail.com>
2024-03-31 16:16:21 +1100

    test(cdk): add s3 bucket for resume

--- infra/aws/lib/infra-stack.ts
+++ infra/aws/lib/infra-stack.ts
@@ -1,16 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new s3.Bucket(this, "resume-bucket");
   }
 }
