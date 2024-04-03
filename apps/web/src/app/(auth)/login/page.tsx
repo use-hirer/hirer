68546b3063f1916633761c7db942835ef3118961
Nick <nick.mandylas@gmail.com>
2024-04-04 00:41:38 +1100

    fix(web): scroll on mobile in login page

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -19,7 +19,7 @@ export default async function AuthenticationPage() {
 
   return (
     <>
-      <div className="container relative h-screen min-h-[600px] flex-col grid place-items-center lg:max-w-none lg:grid-cols-2 lg:px-0 bg-white">
+      <div className="container relative h-[calc(100dvh)] min-h-[600px] flex-col grid place-items-center lg:max-w-none lg:grid-cols-2 lg:px-0 bg-white">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-extrabold gap-1">
