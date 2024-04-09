a15df337f17de20ad34753c66c251fa927db6554
Nick <nick.mandylas@gmail.com>
2024-04-09 22:59:37 +1000

    Squashed commit of the following:
    commit 3a0629d9f5392a15de0332d8361a06cc55a15f23
    Author: Nick <nick.mandylas@gmail.com>
    Date:   Tue Apr 9 22:11:07 2024 +1000
    
        fix(web): parallel routes actually.. working

--- apps/web/src/modules/job/job-tabs.tsx
+++ apps/web/src/modules/job/job-tabs.tsx
@@ -2,8 +2,11 @@
 
 import { Tab, TabGroup, TabList } from "@tremor/react";
 import { useParams, usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const JobTabs: React.FC = () => {
+  const [index, setIndex] = useState(0);
+
   const path = usePathname();
   const { id, slug } = useParams() as { id?: string; slug?: string };
   const pathSegments = path.split("/");
@@ -11,6 +14,16 @@ const JobTabs: React.FC = () => {
   const router = useRouter();
 
   const tabValues = ["/", "/candidates", "/config", "/metrics", "/sharing"];
+
+  useEffect(() => {
+    const defaultIndex = tabValues.findIndex(
+      (value) => value === `/${lastSegment}`
+    );
+
+    if (defaultIndex !== index) {
+      setIndex(defaultIndex);
+    }
+  }, [index, lastSegment, tabValues]);
 
   const defaultIndex = tabValues.findIndex(
     (value) => value === `/${lastSegment}`
@@ -22,18 +35,62 @@ const JobTabs: React.FC = () => {
   };
 
   return (
-    <TabGroup
-      onIndexChange={handleTabChange}
-      defaultIndex={defaultIndex !== -1 ? defaultIndex : 0}
-    >
-      <TabList variant="line" color={"#000"}>
-        <Tab value="/">Overview</Tab>
-        <Tab value="/candidates">Candidates</Tab>
-        <Tab value="/config">Configuration</Tab>
-        <Tab value="/metrics">Metrics</Tab>
-        <Tab value="/sharing">Sharing</Tab>
-      </TabList>
-    </TabGroup>
+    <>
+      <div>
+        <TabGroup defaultIndex={index}>
+          <TabList variant="line" color={"#000"}>
+            <Tab
+              value="/"
+              onClick={(e) => {
+                e.preventDefault();
+                handleTabChange(0);
+              }}
+            >
+              Overview
+            </Tab>
+            <Tab
+              value="/candidates"
+              onClick={(e) => {
+                e.preventDefault();
+                handleTabChange(1);
+              }}
+            >
+              Candidates
+            </Tab>
+            <Tab
+              value="/config"
+              onClick={(e) => {
+                e.preventDefault();
+                handleTabChange(2);
+              }}
+            >
+              Configuration
+            </Tab>
+            <Tab
+              value="/metrics"
+              onClick={(e) => {
+                e.preventDefault();
+                handleTabChange(3);
+              }}
+            >
+              Metrics
+            </Tab>
+            <Tab
+              value="/sharing"
+              onClick={(e) => {
+                e.preventDefault();
+                handleTabChange(4);
+              }}
+            >
+              Sharing
+            </Tab>
+          </TabList>
+        </TabGroup>
+      </div>
+      <div className="my-4">
+        <div className=""></div>
+      </div>
+    </>
   );
 };
 
