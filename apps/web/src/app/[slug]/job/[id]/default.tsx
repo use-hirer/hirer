3be59f5895843ccfe20da03339eda2597e8f54bf
Nick <nick.mandylas@gmail.com>
2024-04-09 19:46:41 +1000

    fix(web): add auth back to job tabs

--- apps/web/src/app/[slug]/job/[id]/default.tsx
+++ apps/web/src/app/[slug]/job/[id]/default.tsx
@@ -1,3 +1,12 @@
-export default function Default() {
+import { validateRequest } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function JobPage() {
+  const { user } = await validateRequest();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
   return null;
 }
