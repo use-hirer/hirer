1774511ab5a660080d6c18d62eda523374e4be16
Nick <nick.mandylas@gmail.com>
2024-04-11 16:56:50 +1000

    begin(web): adding job public page

--- apps/web/src/app/[domain]/page.tsx
+++ apps/web/src/app/[domain]/page.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+
 export default async function ListingPage() {
-  return <div>hello</div>;
+  return (
+    <>
+      <div className="container mt-8 p-2 max-w-[1000px] flex justify-center">
+        <Image src={"/amazon.png"} alt="Amazon Logo" width={100} height={50} />
+      </div>
+      <div className="bg-white shadow-sm h-[calc(100dvh)] container mt-6 rounded-t-2xl p-4 max-w-[1000px]">
+        <div>Company Name</div>
+      </div>
+    </>
+  );
 }
