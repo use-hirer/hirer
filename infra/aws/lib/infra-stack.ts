672328e26247c7946232d8eddf0df048bb81da32
Nick <nick.mandylas@gmail.com>
2024-04-26 12:47:37 +1000

    add(infra): assets bucket + cloudfront

--- infra/aws/lib/infra-stack.ts
+++ infra/aws/lib/infra-stack.ts
@@ -1,11 +1,34 @@
 import * as cdk from "aws-cdk-lib";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
-export class InfraStack extends cdk.Stack {
+export class HirerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, "resume-bucket");
+    const assetsBucket = new s3.Bucket(this, "hirer-assets", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: false,
+      bucketName: "hirer-assets",
+    });
+
+    const distribution = new cloudfront.Distribution(
+      this,
+      "assets-distribution",
+      {
+        defaultBehavior: {
+          origin: new origins.S3Origin(assetsBucket),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      }
+    );
+
+    new cdk.CfnOutput(this, "distribution-domain-name", {
+      value: distribution.distributionDomainName,
+    });
   }
 }
