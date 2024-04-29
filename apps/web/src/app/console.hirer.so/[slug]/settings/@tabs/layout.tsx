19358f1f7119f644c3295be42096437b33852553
Nick <nick.mandylas@gmail.com>
2024-04-29 20:45:48 +1000

    add(web): parallel routes for settings tabs

--- apps/web/src/app/console.hirer.so/[slug]/settings/@tabs/layout.tsx
+++ apps/web/src/app/console.hirer.so/[slug]/settings/@tabs/layout.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { Toaster } from "sonner";
 import TabGroup from "./tab-group";
 
@@ -14,19 +13,7 @@ export default function Layout({
       <div className="flex w-full flex-col">
         <main className="flex flex-1 flex-col gap-4 pt-6">
           <div className="ml-2 grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[140px_1fr]">
-            <nav
-              className="grid gap-4 text-sm text-muted-foreground"
-              x-chunk="dashboard-04-chunk-0"
-            >
-              <Link href="#" className="font-semibold text-primary">
-                General
-              </Link>
-              <Link href="#">Team</Link>
-              <Link href="#">Domains</Link>
-              <Link href="#">Billing</Link>
-              <Link href="#">Support</Link>
-              <TabGroup />
-            </nav>
+            <TabGroup />
             <div className="grid gap-6">{children}</div>
           </div>
         </main>
--- apps/web/src/app/console.hirer.so/[slug]/settings/@tabs/tab-group.tsx
+++ apps/web/src/app/console.hirer.so/[slug]/settings/@tabs/tab-group.tsx
@@ -1,12 +1,60 @@
 "use client";
 
-import { useSelectedLayoutSegment } from "next/navigation";
+import Link from "next/link";
+import { useParams, useSelectedLayoutSegment } from "next/navigation";
+import React from "react";
+
+const tabs = [
+  { title: "General" },
+  { title: "Team", href: "team" },
+  { title: "Domains", href: "domains" },
+  { title: "Billing", href: "billing" },
+  { title: "Support", href: "support" },
+];
 
 const TabGroup: React.FC<{ parallelRoutesKey?: string }> = ({}) => {
-  const loginSegment = useSelectedLayoutSegment();
-  console.log(loginSegment);
+  const segment = useSelectedLayoutSegment();
 
-  return <div>tab group</div>;
+  return (
+    <nav
+      className="grid gap-4 text-sm text-muted-foreground"
+      x-chunk="dashboard-04-chunk-0"
+    >
+      {tabs.map((tab) => (
+        <Tab
+          key={tab.href}
+          href={tab.href}
+          title={tab.title}
+          parallelRoutesKey={segment}
+        />
+      ))}
+    </nav>
+  );
+};
+
+const Tab: React.FC<{
+  href: string | undefined;
+  title: string;
+  parallelRoutesKey?: string | null;
+}> = ({ href, title, parallelRoutesKey }) => {
+  const params = useParams() as { slug: string };
+  const path = `/${params.slug}/settings/` + (href ? href : "");
+  const isActive =
+    // Example home pages e.g. `/layouts`
+    (!href && parallelRoutesKey === null) ||
+    parallelRoutesKey === href ||
+    // Nested pages e.g. `/layouts/electronics`
+    parallelRoutesKey === href;
+
+  if (isActive) {
+    return (
+      <Link href={path} className="font-semibold text-primary">
+        {title}
+      </Link>
+    );
+  }
+
+  return <Link href={path}>{title}</Link>;
 };
 
 export default TabGroup;
