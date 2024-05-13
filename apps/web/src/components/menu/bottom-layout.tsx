0b94dfdb5b67501f42514c0fcfe2b803e09f56b6
Nick <nick.mandylas@gmail.com>
2024-05-14 00:49:33 +1000

    fix(web): remove alpha message when side bar is collapsed

--- apps/web/src/components/menu/bottom-layout.tsx
+++ apps/web/src/components/menu/bottom-layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@hirer/ui";
 import { Gear, Info } from "@phosphor-icons/react";
 import { useParams, usePathname } from "next/navigation";
 import React from "react";
@@ -30,7 +31,12 @@ const BottomMenuLayout: React.FC<{ collapsed: boolean }> = ({ collapsed }) => {
 
   return (
     <>
-      <div className="p-4 border border-orange-300 rounded-md bg-orange-100 mb-3 text-xs">
+      <div
+        className={cn([
+          "p-4 border border-orange-300 rounded-md bg-orange-100 mb-3 text-xs",
+          collapsed && "hidden",
+        ])}
+      >
         Hirer is in Alpha. We&apos;re actively working on improving the
         experience. If you have any feedback, please reach out to us on{" "}
         <a
