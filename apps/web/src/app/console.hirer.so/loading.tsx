3870c381370449eb4ff063c6d5619eb385482b8f
Nick <nick.mandylas@gmail.com>
2024-06-10 22:57:11 +0900

    add(web): console loading page

---
+++ apps/web/src/app/console.hirer.so/loading.tsx
@@ -0,0 +1,11 @@
+import { SpinnerGap } from "@phosphor-icons/react/dist/ssr";
+
+export default function Loading() {
+  return (
+    <div className="flex w-full h-full min-h-[calc(100dvh)] flex-col items-center justify-center">
+      <div className="flex items-center justify-center w-8 h-8 bg-black text-white rounded-full text-md font-extrabold animate-spin">
+        <SpinnerGap />
+      </div>
+    </div>
+  );
+}
